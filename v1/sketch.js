338b564af4795e098a9b3eef6f39240b8ec00305
maikeklemm <48826824+maikeklemm@users.noreply.github.com>
2021-06-18 16:39:57 +0200

    sadfghjk

--- v1/sketch.js
+++ v1/sketch.js
@@ -1,22 +1,62 @@
 
 var mic;
-// var amp;
-// var volhistory = [];
-
+var amp;
+var volhistory = [];
+var video;
+var memoryVideos = [['../input/video_2.mp4'],['../input/video_1.mp4'],['../input/video_4.mp4']]
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	// createCanvas(400, 400);
+
+
+	var cnv = createCanvas(windowWidth, windowHeight);
+	// cnv.style('display', 'block');
+	cnv.style('display', 'inline');
+	
+
 	mic = new p5.AudioIn();
 	mic.start();
-	// amp = new p5.Amplitude();
+	amp = new p5.Amplitude();
+
+	
+
+	//video
+
+	// video = createCapture(VIDEO);
+
+	video = createVideo (memoryVideos[Math.floor(Math.random() * memoryVideos.length)], vidLoad);
+	video.size(windowWidth,windowHeight);
+	video.hide();
+	console.log("video" );
+
+
+	// resetSketch();
 }
 
+// function createVideo (){
+
+// 	video = createVideo (memoryVideos[Math.floor(Math.random() * memoryVideos.length)], vidLoad);
+// 	video.size(windowWidth,windowHeight);
+// 	console.log("video" );
+
+//  }
+
+
+function vidLoad() {
+	video.loop();
+	video.volume(0);
+  }
+
+  
+
 function draw() {
-	background(0);
+	// background(0);
+
+
+	image(video, 0, 0)
+
 	var vol = mic.getLevel();
 
-	// volhistory.push(vol);
+	volhistory.push(vol);
 	// stroke(255);
 	// noFill();
 	// beginShape();
@@ -25,14 +65,61 @@ function draw() {
 	// 	vertex(i, y);
 	// }
 	// endShape();
-	// if (volhistory.lenght > width) {
+	// if (volhistory.lenght > windowWidth) {
 	// 	volhistory.splice(0, 1);
 	// }
 
 
-	ellipse (windowWidth/2, windowHeight/2, vol*10000 , vol*10000);
+	var positionX = windowHeight*(2*(-vol));
+	translate(0,windowHeight-(1/5*windowHeight));
+	console.log(positionX);
+
+if (positionX > -300) { 						
+	noStroke();
+	fill(255, 204, 0);
+	ellipse (windowWidth/2, positionX , 100 , 100);
+
+ } 
+
+
+ else { 
+	noStroke();
+	fill(255, 100, 0);
+	ellipse (windowWidth/2, positionX , 150 , 150);
+	
+	background('rgba(0%,0%,0%,0.25)');
+	// setTimeout(resetSketch, 3000);
+	resetSketch();
+
+
+	// for (i = 0; positionX > -130; i++) {
+	// 	background('rgba(0%,0%,0%,0.25)')
+	//   }
+	
+  } 
+
+function resetSketch(){
+
+	video = createVideo (memoryVideos[Math.floor(Math.random() * memoryVideos.length)], vidLoad);
+	video.size(windowWidth,windowHeight);
+	video.hide();
+	console.log("new video")
+}
+
+
+	// // var positionX = (windowHeight/5+(windowHeight/2))*(5*(-vol));
+	// var positionX = windowHeight*(2*(-vol));
+
+	// translate(0,windowHeight-(1/5*windowHeight));
+
+	// // ellipse (windowWidth/2, windowHeight/2, vol*10000 , vol*10000);
+	// ellipse (windowWidth/2, positionX , 100 , 100);
+
+
+
 
 	console.log(vol);
 
 
 }
+
